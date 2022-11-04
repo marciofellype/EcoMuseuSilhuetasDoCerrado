dd2a1516f10954b44feb8869401f1de32da36556
marciofellype <37717831+marciofellype@users.noreply.github.com>
2022-11-04 11:32:28 -0300

    Criação do javascrip
    Add efeito ao rolar a tela

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+window.sr = ScrollReveal({ reset:true });
+
+sr.reveal('.wrapper',{duration: 2000})
+
+sr.reveal('.container', {
+    rotate: { x:0, y:80, z:0 },
+    duration: 2000
+})
+
+sr.reveal('.title', {
+    rotate: { x:0, y:80, z:0 },
+    duration: 2000
+})
